42a892c24462ebc17206d29f153cc6e85325b3c8
Yasemen Küçükoğlu <67151151+ykucukoglu@users.noreply.github.com>
2026-04-09 22:01:57 +0300

    add dotween and brick effect

--- Assets/_project/Scripts/Elements/Brick.cs
+++ Assets/_project/Scripts/Elements/Brick.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System;
 using UnityEngine;
 
@@ -5,16 +6,33 @@ public class Brick : MonoBehaviour
 {
     public int startHealth;
     private int _currentHealth;
-
+    private Color _stepValue;
+    public SpriteRenderer sprite;
+    public float colorStep;
     private void Start()
     {
         _currentHealth = startHealth;
+        var greenandblueValue = 1 - _currentHealth * colorStep;
+        sprite.color = new Color(1, greenandblueValue, greenandblueValue, 1);
     }
 
     public void GetHit()
     {
         _currentHealth--;
+        var greenandblueValue = 1 - _currentHealth * colorStep;
+
+        PlayVisualFX(greenandblueValue);
         if (_currentHealth <= 0) DestroyBrick();
+    }
+
+    private void PlayVisualFX(float greenandblueValue)
+    {
+        sprite.transform.DOKill();
+        sprite.transform.localScale = Vector3.one * .2f;
+        sprite.transform.localPosition = Vector3.zero;
+        sprite.transform.DOScale(.23f, .05f).SetLoops(2, LoopType.Yoyo);
+        sprite.DOColor(new Color(1, greenandblueValue, greenandblueValue, 1), .1f);
+        sprite.transform.DOPunchPosition(Vector3.one * .1f, .1f, 100);
     }
 
     private void DestroyBrick()
